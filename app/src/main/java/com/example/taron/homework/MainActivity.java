7fe01d38d5bcab84fefd253a8d936b002ee37abf
Tigran Gevorgyan <tigrangevorgyan95@gmail.com>
2017-04-27 15:23:43 +0400

    Initial commit

---
+++ app/src/main/java/com/example/taron/homework/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.taron.homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        TextView textView = (TextView)findViewById(R.id.activity_tv1);
+
+        textView.setText("aadasd");
+
+    }
+}
